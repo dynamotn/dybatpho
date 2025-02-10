21496089cee54a93c6c0ea8f617d733fef91295b
Trần Đức Nam <me@dynamotn.dev>
2025-02-10 15:27:53 +0700

    feat: add some string functions

--- src/string.sh
+++ src/string.sh
@@ -2,6 +2,69 @@
 : "${DYBATPHO_DIR:?DYBATPHO_DIR must be set. Please source dybatpho/init.sh before other scripts from dybatpho.}"
 
 #######################################
+# Trim leading and trailing white-space from string.
+# Arguments:
+#   1: string to change
+# Outputs:
+#   Trimmed string
+#######################################
+# shellcheck disable=SC2317
+_trim() {
+  : "${1#"${1%%[![:space:]]*}"}"
+  : "${_%"${_##*[![:space:]]}"}"
+  printf '%s\n' "$_"
+}
+
+#######################################
+# Split a string on a delimiter.
+# Arguments:
+#   1: string to split
+#   2: delimiter
+# Outputs:
+#   Show each part of splited string
+#######################################
+_split() {
+  IFS=$'\n' read -d "" -ra arr <<< "${1//$2/$'\n'}"
+  printf '%s\n' "${arr[@]}"
+}
+
+#######################################
+# URL-encode a string.
+# Arguments:
+#   1: string to encode
+# Outputs:
+#   Encoded string
+#######################################
+_url_encode() {
+  local LC_ALL=C
+  for (( i = 0; i < ${#1}; i++ )); do
+    : "${1:i:1}"
+    case "$_" in
+      [a-zA-Z0-9.~_-])
+        printf '%s' "$_"
+        ;;
+
+      *)
+        printf '%%%02X' "'$_"
+        ;;
+    esac
+  done
+  printf '\n'
+}
+
+#######################################
+# URL-decode a string.
+# Arguments:
+#   1: string to decode
+# Outputs:
+#   Decoded string
+#######################################
+_url_decode() {
+  : "${1//+/ }"
+  printf '%b\n' "${_//%/\\x}"
+}
+
+#######################################
 # Convert a string to lowercase.
 # Arguments:
 #   1: string to change
@@ -21,4 +84,15 @@ _lower() {
 #######################################
 _upper() {
   printf '%s\n' "${1^^}"
+}
+
+#######################################
+# Reverse a string case.
+# Arguments:
+#   1: string to convert
+# Outputs:
+#   Converted string
+#######################################
+_reverse() {
+  printf '%s\n' "${1~~}"
 }
--- test/string.bats
+++ test/string.bats
@@ -4,6 +4,45 @@ setup() {
   source "${DYBATPHO_DIR}/init.sh"
 }
 
+@test "_trim output string" {
+  run _trim "	Hello,   dybatpho   "
+  assert_success
+  assert_output "Hello,   dybatpho"
+}
+
+@test "_split output string" {
+  run _split "apples,oranges,pears,grapes" ","
+  assert_success
+  assert_output << EOF
+apples
+oranges
+pears
+grapes
+EOF
+  run _split "hello---world---my---name---is---dynamo" ","
+  assert_success
+  assert_output << EOF
+hello
+world
+my
+name
+is
+dynamo
+EOF
+}
+
+@test "_url_encode output string" {
+  run _url_encode "https://github.com/dynamotn/dybatpho/?f=This is sample string"
+  assert_success
+  assert_output "https%3A%2F%2Fgithub.com%2Fdynamotn%2Fdybatpho%2F%3Ff%3DThis%20is%20sample%20string"
+}
+
+@test "_url_decode output string" {
+  run _url_decode "https%3A%2F%2Fgithub.com%2Fdynamotn%2Fdybatpho%2F%3Ff%3DThis%20is%20sample%20string"
+  assert_success
+  assert_output "https://github.com/dynamotn/dybatpho/?f=This is sample string"
+}
+
 @test "_lower output string" {
   run _lower "dYbaTPHO"
   assert_success
@@ -14,4 +53,10 @@ setup() {
   run _upper "dYbaTPHO"
   assert_success
   assert_output "DYBATPHO"
+}
+
+@test "_reverse output string" {
+  run _reverse "dYbaTPHO"
+  assert_success
+  assert_output "DyBAtpho"
 }
